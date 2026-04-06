7bfcdfcc0feb08a1d7123fdffe202d7649fb2342
Felipe Menezes <fm08102003@gmail.com>
2026-04-06 14:55:19 -0300

    estrutura correta do projeto

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "API online"}
+
+@app.get("/soma/{a}/{b}")
+def soma(a: int, b: int):
+    return {"resultado": a + b}
