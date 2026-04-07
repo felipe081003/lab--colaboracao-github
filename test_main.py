34d5d7588280aabbc76c7ccf86e665da8c66e295
Felipe Menezes <fm08102003@gmail.com>
2026-04-07 14:41:09 -0300

    fix: corrigindo app e testes multiplicaco

--- test_main.py
+++ test_main.py
@@ -1,4 +1,14 @@
-def test_multiplicar():
-    response = client.get("/multiplicar/2/2")
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"resultado": 5}  # errado de propósito
+    assert response.json() == {"mensagem": "API funcionando!"}
+
+def test_somar():
+    response = client.get("/somar/5/3")
+    assert response.status_code == 200
+    assert response.json() == {"resultado": 8}
